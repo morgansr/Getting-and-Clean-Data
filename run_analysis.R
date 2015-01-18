5fb639fb6c531f162aabe6d2a38ae6138dc0d3a1
Scott <smorgan84@gmail.com>
2015-01-18 17:48:58 -0500

    showing off

--- run_analysis.R
+++ run_analysis.R
@@ -19,7 +19,7 @@ run_analysis <- function() {
     ## Appropriately labels the data set with descriptive variable names. 
     ## From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-    
+    ## Julia is a nice young lady
 
 
 }
